fe91bfbf7cf33adeae085c252cb03dae12f437df
Ivan Santiago Vargas <santi061921@gmail.com>
2018-07-21 13:58:53 -0500

    Guardar en la BD

--- proyecto1/app/controllers/event_controller.rb
+++ proyecto1/app/controllers/event_controller.rb
@@ -1,4 +1,27 @@
 class EventController < ApplicationController
   def get_location
+  	@location = Event.last(10)
+end
+
+def store
+	respond_to do |format|
+
+		latitude = params[:latitude]
+		longitude = params[:longitude]
+
+		@event = Event.new(user: 1, x: longitude, y: latitude)
+
+		if @event.save then 
+
+			format.html{redirect_to event_get_location_path}
+
+		else
+
+			flash[:notice_booking_fail] =  true 
+			redirect_to event_get_location_path
+		end	
+	end	
+
+
   end
 end
--- proyecto1/config/routes.rb
+++ proyecto1/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'event/get_location'
+  get 'event/store'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
